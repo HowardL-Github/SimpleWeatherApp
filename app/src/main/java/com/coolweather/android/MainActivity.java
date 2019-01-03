00ee9d5df26509cdaa3b17dec118f7995aa6d536
lnqf <46151629+lnqf@users.noreply.github.com>
2019-01-03 16:19:56 +0800

    Update MainActivity.java

--- app/src/main/java/com/coolweather/android/MainActivity.java
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -13,7 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //先从SharedPreferences文件中读取缓存数据
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //如果之前请求过天气数据，无需选择城市，直接跳转到WeatherActivity
         if (prefs.getString("weather", null) != null) {
             Intent intent = new Intent(this, WeatherActivity.class);
             startActivity(intent);
