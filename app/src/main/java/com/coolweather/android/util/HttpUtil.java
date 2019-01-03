75ebb1cbd3aa399ebcdc2b5d06e03c5e8f587d83
lnqf <46151629+lnqf@users.noreply.github.com>
2019-01-03 16:18:04 +0800

    Update HttpUtil.java

--- app/src/main/java/com/coolweather/android/util/HttpUtil.java
+++ app/src/main/java/com/coolweather/android/util/HttpUtil.java
@@ -3,12 +3,12 @@ package com.coolweather.android.util;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 
-public class HttpUtil {
+public class HttpUtil {  //从服务器获取全国省市县数据
 
-    public static void sendOkHttpRequest(String address, okhttp3.Callback callback) {
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback) {  //调用sendOkHttpRequest()方法
         OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder().url(address).build();
-        client.newCall(request).enqueue(callback);
+        Request request = new Request.Builder().url(address).build();  //传入请求地址
+        client.newCall(request).enqueue(callback);  //注册回调处理服务器响应
     }
 
 }
